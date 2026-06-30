094ea92430939f7b1a6e9abccdefb1193b0838b1
jojo <jimy.bovis@gmail.com>
2026-06-30 11:20:44 -0300

    update app

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # ======================
 # CONFIG
 # ======================
-OBJECTIF_KM = 300
+OBJECTIF_KM = 15
 FICHIER = "data.json"
 
 # ======================
@@ -30,18 +30,70 @@ data = charger_donnees()
 # ======================
 # INTERFACE
 # ======================
-st.title("🏃‍♂️ Janouille Running Motivation")
+st.title("🏃‍♂️ Jajou Running Motivation")
+st.write("Si tu ne vas pas courir, la loutre va mourir de faim !")
+st.title("🦦")
+st.write("Objectif : ", OBJECTIF_KM, "km par semaine")
 
-st.write("Objectif : ", OBJECTIF_KM, "km")
+if "show_gif" not in st.session_state:
+    st.session_state["show_gif"] = False
 
-# Progression
-progression = data["total_km"] / OBJECTIF_KM
-if progression > 1:
-    progression = 1
+if st.session_state["show_gif"]:
+    st.image("https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3VtOTRuem4zNDFncDN4d3o3ZjdxM2diZDlrM2h5NTNsYnJuNjduYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11p8Lr9eVeXq2Q/giphy.gif")
+    st.success("🏃‍♂️ Course ajoutée !")
 
-st.progress(progression)
+    st.session_state["show_gif"] = False
 
-st.write(f"### {data['total_km']} / {OBJECTIF_KM} km")
+
+
+
+progress_percent = min(data["total_km"] / OBJECTIF_KM, 1) * 100
+
+st.markdown(f"""
+<div style="position: relative; width: 100%; background-color: #eee; border-radius: 10px; height: 25px;">
+  
+  <div style="
+    width: {progress_percent}%;
+    background-color: #4CAF50;
+    height: 100%;
+    border-radius: 10px;
+    position: relative;
+  ">
+    <span style="
+      position: absolute;
+      right: -15px;
+      top: -10px;
+      font-size: 20px;
+    ">
+      🥕
+    </span>
+  </div>
+
+  <!-- loutre -->
+  <div style="
+    position: absolute;
+    right: -10px;
+    top: -12px;
+    font-size: 28px;
+  ">
+    🦦
+  </div>
+
+
+</div>
+
+<p>{int(progress_percent)}% — {data['total_km']} / {OBJECTIF_KM} km</p>
+""", unsafe_allow_html=True)
+
+
+if data["total_km"] >= OBJECTIF_KM:
+    st.image(
+        "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjNzbzRsemc1cDQ3cTNhMTZhdXhtZzl0bnBwcm1udzdnaW4yZHd5cSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/N52vvvIAO4SYDvTNZY/giphy.gif",
+        caption="🎉 Objectif atteint ! La loutre est fière de toi 🦦"
+    )
+
+
+
 
 # ======================
 # AJOUT KM
@@ -59,7 +111,24 @@ if st.button("Ajouter"):
     })
 
     sauvegarder_donnees(data)
+
+    st.session_state["show_gif"] = True
     st.rerun()
+
+
+
+# ======================
+# RESET
+# ======================
+
+st.subheader("Reset")
+
+if st.button("Réinitialiser tout"):
+    data["total_km"] = 0
+    data["historique"] = []
+    sauvegarder_donnees(data)
+    st.rerun()
+
 
 # ======================
 # HISTORIQUE
@@ -74,11 +143,11 @@ for course in reversed(data["historique"]):
 # ======================
 st.subheader("Badges")
 
-if data["total_km"] >= 50:
-    st.success("🥉 Bronze runner")
+if data["total_km"] >= 5:
+    st.success("🥉 Bien joué ma Petite Loutre, fonce !")
 
-if data["total_km"] >= 150:
-    st.success("🥈 Silver runner")
+if data["total_km"] >= 10:
+    st.success("🥈 ça c'est ma copine à moi !")
 
-if data["total_km"] >= 300:
-    st.success("🥇 Gold runner")
+if data["total_km"] >= 15:
+    st.success("🥇 Trop forte, je suis fier de toi mon ptit citron 🍋")
